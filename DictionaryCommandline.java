5141ca85ba8252363016f30e2dfcd4af4f51dbc8
Nguyen Thi Thanh Thuy <thanhthuy692002@gmail.com>
2021-09-29 17:09:32 +0700

    DicCom

---
+++ DictionaryCommandline.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class DictionaryCommandline {
+    public void showAllWord(int n, Word[] newWord) {
+        System.out.println("No" + "         " + "|English" + "     " + "|Vietnamese") ;
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i + "     |" + newWord[i - 1].getWord_target() + "     |" + newWord[i - 1].getWord_explain());
+        }
+    }
+}
