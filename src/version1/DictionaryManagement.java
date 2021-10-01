c01302300191bc3a788efd1d86c84ded2ef344e4
magneto <83504674+magnetto@users.noreply.github.com>
2021-10-01 21:57:14 +0700

    Update DictionaryManagement.java
    Fix bugs output method insertFromCommandline()

--- src/version1/DictionaryManagement.java
+++ src/version1/DictionaryManagement.java
@@ -12,25 +12,27 @@ public class DictionaryManagement {
         for (int i = 0; i < numOfWords; i++) {
             Word newWord = new Word();
             System.out.println("No " + (i + 1) + ": ");
-            String temp = input.nextLine();
+            if (i == 0) {
+                String begin = input.nextLine();
+            }
             System.out.print("Word_target: ");
             newWord.setWord_target(input.nextLine());
             System.out.print("Word_explain: ");
-            newWord.setWord_explain(input.next());
+            newWord.setWord_explain(input.nextLine());
             dictionary.wordList.add(newWord);
         }
     }
 
     void showAllWords() {
-        System.out.printf("%-15s", "No");
-        System.out.printf("%-30s", "|English");
-        System.out.printf("%-30s", "|Vietnamese");
+        System.out.printf("%-8s", "No");
+        System.out.printf("%-24s", "|English");
+        System.out.printf("%-24s", "|Vietnamese");
         System.out.println();
 
         for (int i = 0; i < dictionary.wordList.size(); i++) {
-            System.out.printf("%-15s", i + 1);
-            System.out.printf("%-30s", dictionary.wordList.get(i).getWord_target());
-            System.out.printf("%-30s", dictionary.wordList.get(i).getWord_target());
+            System.out.printf("%-8s", i + 1);
+            System.out.printf("%-24s", "|" + dictionary.wordList.get(i).getWord_target());
+            System.out.printf("%-24s", "|" + dictionary.wordList.get(i).getWord_explain());
             System.out.println();
         }
     }
